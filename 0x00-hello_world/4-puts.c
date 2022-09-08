66fd4e8358ba582f85e09d2feeaa2c4b734136b9
Nemo4990 <nemomsadereje123@gmail.com>
2022-09-08 09:11:20 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entery point 
+ *
+ * Return : 0 after printing the function
+ */
+it main (void)
+
+	rintf("\" programing is like building a multilingual puzzle");
+	return(0);
+}
+
